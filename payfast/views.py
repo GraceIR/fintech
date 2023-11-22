f4648e3a31f910912dfc5e461f3bd66c3dc9ba62
Grace Onyenaturuchi <graceonyenaturuchi11@gmail.com>
2023-11-22 16:37:17 +0100

    fintech

--- payfast/views.py
+++ payfast/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+#from .models import payfast
 
 def home(request):
-    return render(request, 'home.html')
-    
-    
+    return render(request,'home.html')
+
+
+def send_money(request):
+   sender = request.form['sender']
+   receiver = request.form['receiver']
+   amount = int(request.form['amount'])
+
+   if sender not in users or receiver not in users:
+       return "Invalid sender or receiver"
+
+       if user[sender]['balance']< amount:
+           return "Insufficient funds"
+
+           return redirect(url_for('home'))
 
 
 
