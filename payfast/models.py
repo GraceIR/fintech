2e9b6ba1302051425953d2d13e4d051594369581
dee-engineer <chukwudivineify@gmail.com>
2023-11-24 16:07:36 +0100

    added home page

--- payfast/models.py
+++ payfast/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from payfast.manager import UserManager
 
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=True, null=True)
+    username = models.CharField(max_length=200, unique=True, blank=True, null=True)
+    USERNAME_FIELD = 'email'
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def get_full_name(self) -> str:
+        return super().get_full_name()
+    
 
--- payfast/views.py
+++ payfast/views.py
@@ -1,22 +1,10 @@
-from django.shortcuts import render
-#from .models import payfast
+from django.shortcuts import render, redirect
+
 
 def home(request):
     return render(request,'home.html')
 
 
-def send_money(request):
-   sender = request.form['sender']
-   receiver = request.form['receiver']
-   amount = int(request.form['amount'])
-
-   if sender not in users or receiver not in users:
-       return "Invalid sender or receiver"
-
-       if user[sender]['balance']< amount:
-           return "Insufficient funds"
-
-           return redirect(url_for('home'))
-
-
+def dash(request):
+    return render(request,'dash.html')
 
