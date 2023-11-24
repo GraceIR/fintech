654e493952d9a09afaaf7b2c14f9125670fdb303
dee-engineer <chukwudivineify@gmail.com>
2023-11-24 13:28:01 +0100

    updated exclusion of virtual environment

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gracie/fintech/venv"
+set -gx VIRTUAL_ENV "/home/divine/Desktop/fintech/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
